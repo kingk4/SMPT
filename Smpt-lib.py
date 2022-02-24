6e93ef0fdc6a3c33e6add48d3487263029e84968
kingk4 <92218590+kingk4@users.noreply.github.com>
2022-02-24 19:04:36 +0500

    Update Smpt-lib.py

--- Smpt-lib.py
+++ Smpt-lib.py
@@ -2,5 +2,5 @@ import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('p200179@pwr.nu.edu.pk', 'Kalilinix99')
+server.login('p200179@pwr.nu.edu.pk', '{Enter your passwd}')
 server.sendmail('p200179@pwr.nu.edu.pk', 'dj835048@gmail.com', 'Hi💕dj! I Love ❤️You ')
